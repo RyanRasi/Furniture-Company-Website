d8bb37a6bc8e550e9de615f493efdcb4a7de3a0f
RyanRasi <theryanrasi@gmail.com>
2020-05-06 00:35:50 +0100

    removed GSAP libraries and animation

--- index.php
+++ index.php
@@ -26,27 +26,18 @@ $_SESSION['signupError'] = NULL;
 </head>
 
 <body>
+<!-- Imports the Navbar -->
   <?php include "./components/navbar.php"; ?>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha256-lPE3wjN2a7ABWHbGz7+MKBJaykyzqCbU96BJWjio86U=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js" integrity="sha256-fIkQKQryItPqpaWZbtwG25Jp2p5ujqo/NwJrfqAB+Qk=" crossorigin="anonymous"></script>
-
-  <header>
-    <section>
-      <div class="containerSlide">
-        <img src="./img/sofa.jpg" alt="An image of a blue sofa">
-        <h1 class="headline">Your Style!</h1>
-      </div>
-    </section>
-  </header>
-
-  <div class="slider"></div>
+  <!-- The intro picture -->
+  <section>
+  <img src="./img/sofa.jpg" alt="An image of a blue sofa">
+  <h1 class="headline">YOUR STYLE!</h1>
+  </section>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-  <script src="./script/furnitureSlideIn.js"></script>
 </body>
 
 </html>
