2240670f5a63d57257caa6a5cec132fd216ae808
RyanRasi <theryanrasi@gmail.com>
2020-05-06 00:48:37 +0100

    Database Credentials File

---
+++ databaseConfig/credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+// specify your own database credentials
+ $host = "localhost";
+ //private $db_name = "PHPLearning";
+ $username = "root";
+ $password = "";
+ $dbname = "17010485";
+
+ ?>
